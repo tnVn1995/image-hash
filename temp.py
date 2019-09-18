9b65a21c4e7a49bf26236ce005b50a1c5918a99c
tung2921 <35050694+tung2921@users.noreply.github.com>
2019-09-18 14:45:23 -0500

    Update temp.py
    Update codes for Pixabay image search

--- temp.py
+++ temp.py
@@ -13,10 +13,11 @@ URL = 'https://pixabay.com/api/'
 params = {'key': API_KEY, 'q': term, 'per_page': 200, 'image_type': 'photo'}
 
 search = requests.get(URL, params = params)
+search.raise_for_status()
+
 
 results = search.json()
 
-results.keys()
 
 totalHits = results['totalHits']
 
@@ -26,3 +27,20 @@ print(f'total results for {term} are {totalHits}')
 
 total = 0
 
+for i in range(1,totalHits/len(photos)):
+    print(f'[INFO] making request for {i}th page from the search query ...')
+    params = {'key': API_KEY, 'q': term, 'per_page': 200, 'image_type': 'photo', 'page': i}
+    search = requests.get(URL, params = params)
+    search.raise_for_status()
+    
+    results = search.json()
+    print(f'Saving images for the {i}th page')
+    
+    for im in results['hits']:
+        try:
+            print(f"Fetching {im['webformatURL']}")
+            r = requests.get(im['webformatURL'], timeout=30)
+            
+            ext = im['webformatURL']im['webformatURL'].rfind("."):]
+            p = 
+        
